478a05b0268a30836c33ba0ea9fd82636f1454ee
Krishna jha <129253790+Krishnauff@users.noreply.github.com>
2023-04-15 12:35:00 +0530

    Update dev.py

--- FallenRobot/modules/dev.py
+++ FallenRobot/modules/dev.py
@@ -18,7 +18,7 @@ from FallenRobot.modules.helper_funcs.chat_status import dev_plus
 def allow_groups(update: Update, context: CallbackContext):
     args = context.args
     if not args:
-        update.effective_message.reply_text(f"Current state: {FallenRobot.ALLOW_CHATS}")
+        update.effective_message.reply_text(f"Current state: {tanurosebot.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
         FallenRobot.ALLOW_CHATS = True
