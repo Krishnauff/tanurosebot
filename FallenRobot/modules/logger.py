94b39216d19289deba04b4f3124a834d0d5e4a7c
Krishna jha <129253790+Krishnauff@users.noreply.github.com>
2023-05-11 16:57:34 +0530

    Update logger.py

--- FallenRobot/modules/logger.py
+++ FallenRobot/modules/logger.py
@@ -1,6 +1,6 @@
 from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
-from AnonX import app
-from AnonX.utils.database import is_on_off
+from FallenRobot import app
+from FallenRobot.utils.database import is_on_off
 
 
 async def play_logs(message, streamtype):
